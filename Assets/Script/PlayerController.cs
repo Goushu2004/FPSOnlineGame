8ce987662db1dc853e14f359376864d6a645c175
沟鼠 <goushu2004@Gmail.com>
2026-06-01 19:22:07 +0800

    Latest

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -2,25 +2,62 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.XR;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 public class PlayerController : MonoBehaviour
 {
-    private Vector2 playerInput;
-    private float moveSpeed = 5f;
+    private Rigidbody rb;
+    private float horizontalInput;
+    private float verticalInput;
+    Vector3 velocity;
+    private float moveSpeed = 2f;
+    private Animator playerAnimator;
+    public GameObject playerPosition;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
+        playerAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 moveDirection = transform.right * playerInput.x + transform.forward * playerInput.y;
+        SetAnimatorParameters();
     }
-    public void OnMove(InputValue value) 
+    private void FixedUpdate()
     {
-        if (value != null) 
+        Vector3 moveDirection = (playerPosition.transform.forward * verticalInput + playerPosition.transform.right * horizontalInput).normalized;
+        velocity = moveDirection * moveSpeed;
+        rb.linearVelocity = new Vector3(velocity.x, rb.linearVelocity.y, velocity.z);
+    }
+    public void OnMove(InputAction.CallbackContext context) 
+    {
+        if (context.performed)
         {
-            playerInput = value.Get<Vector2>();
+            Vector2 input = context.ReadValue<Vector2>();
+            horizontalInput = input.x;
+            verticalInput = input.y;
         }
+        else if (context.canceled)
+        {
+            horizontalInput = 0f;
+            verticalInput = 0f;
+        }
+    }
+    public void OnCourch(InputAction.CallbackContext context) 
+    {
+        if (context.performed)
+        {
+            playerAnimator.SetBool("IsCourching", true);
+        }
+        else if (context.canceled)
+        {
+            playerAnimator.SetBool("IsCourching", false);
+        }
+    }
+    public void SetAnimatorParameters() 
+    {
+        playerAnimator.SetFloat("VelocityX", horizontalInput);
+        playerAnimator.SetFloat("VelocityZ", verticalInput);
     }
 }
